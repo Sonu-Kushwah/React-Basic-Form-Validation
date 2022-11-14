8ddb9c3bbc42fed1abbccf7e9b7e6e7eb808829e
learnReactgithub <sonukushwah362@gmail.com>
2022-11-14 14:16:33 +0530

    react basic form validation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState();
+  const [password, setPasswod] = useState();
+  const [userErr, setUserErr] = useState();
+  const [passErr, setPasswodErr] = useState();
+
+  const loginHandle = (e) => {
+    if(user.length<3 || password.length<3){
+      alert("type correct value");
+    }else{
+      alert("all is good");
+    }
+    e.preventDefault();
+  };
+  const userHandle=(e)=>{
+   let item=e.target.value;
+   if(item.length<3){
+     setUserErr(true);
+   }else
+    setUserErr(false);
+    setUser(item);
+  }
+  const passHandle=(e)=>{
+   let item=e.target.value;
+   if(item.length<3){
+    setPasswodErr(true);
+   }else{
+    setPasswodErr(false);
+    setPasswod(item);
+   }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>hide and show</h2>
+      <form onSubmit={loginHandle}>
+        <input type="text" placeholder="Enter Your Name" onChange={userHandle}/>
+        <br />
+        {userErr ? <span>User Not Valid</span>:""}<br/>
+        <input type="password" placeholder="Enter Your Password" onChange={passHandle}/>
+        <br />
+        {passErr? <span>Passw Not Valid</span> :" "}<br/>
+        <button type="submit">submit</button>
+      </form>
+    </>
   );
 }
 
